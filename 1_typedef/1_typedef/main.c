b773db84fb49f3a007dc78b2b1a9dfd4fba800bf
snake <snake@cosyjoy.com>
2018-08-27 20:32:15 +0800

    typedef在结构体的使用

---
+++ 1_typedef/1_typedef/main.c
@@ -0,0 +1,28 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Student
+{
+	char name[100];
+
+	int  * age;
+}STUDENT;
+
+
+
+
+int main() 
+{
+	int a = 10000;
+
+	//printf("============%d",a);
+	STUDENT std = { "snake",a};
+
+	printf("value %s   %d" ,std.name,10000);
+	system("pause");
+	return EXIT_SUCCESS;
+}
